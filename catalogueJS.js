032a767c2c7d5f0b10e6cea3fddee5781af1628f
marouanenili <58077859+marouanenili@users.noreply.github.com>
2022-04-24 21:19:13 +0200

    Add files via upload

---
+++ catalogueJS.js
@@ -0,0 +1,443 @@
+const liste_de_piece=[
+
+  model_piece={
+    designation:"Chappe de Direction",
+    prix:"60.00",
+    image_path:"img\\1.jpeg",
+    reference:"0160206129",
+    marque:"MEYLE",
+    cout:"52.95"
+  },
+  model_piece={
+    designation:"KIT S.BLOC BMW",
+    prix:"300.00",
+    image_path:"img\\2.jpeg",
+    reference:"3146100000/HD",
+    marque:"MEYLE",
+    cout:"238.00"
+  },
+  model_piece={
+    designation:"KIT N.BARRE SECOUSSE M190",
+    prix:"60.00",
+    image_path:"img\\3.jpeg",
+    reference:"1130801",
+    marque:"LEMFORDER",
+    cout:"50"
+  },
+  model_piece={
+    designation:"KIT N.BARRE SECOUSSE M250",
+    prix:"60.00",
+    image_path:"img\\4.jpeg",
+    reference:"1111701",
+    marque:"LEMFORDER",
+    cout:"47"
+  },
+  model_piece={
+    designation:"SUPPORT MOTEUR M240",
+    prix:"85.00",
+    image_path:"img\\5.jpeg",
+    reference:"0140240015",
+    marque:"MEYLE",
+    cout:"72.00"
+  },
+  model_piece={
+    designation:"BOUGIE ESSENCE &14 LONG",
+    prix:"8.00",
+    image_path:"img\\6.jpeg",
+    reference:"F7TC",
+    marque:"BIGS",
+    cout:"6.00"
+  },
+  model_piece={
+    designation:"FILTRE A GAZOIL GOLF7/ CADDY4",
+    prix:"120.00",
+    image_path:"img\\7.jpeg",
+    reference:"A120922",
+    marque:"DENCKERMANN",
+    cout:"100.00"
+  },
+  model_piece={
+    designation:"FILTRE A GASOIL T203",
+    prix:"90.00",
+    image_path:"img\\8.jpeg",
+    reference:"A120058",
+    marque:"DENCKERMANN",
+    cout:"78.00"
+  },
+  model_piece={
+    designation:"FILTRE A GAZOIL GOLF5/A3 1.6 DKR",
+    prix:"70.00",
+    image_path:"img\\9.jpeg",
+    reference:"A120317",
+    marque:"DENCKERMANN",
+    cout:"60.00"
+  },
+  model_piece={
+    designation:"FILTRE GAZOIL TOUAREG / PORCHE",
+    prix:"85.00",
+    image_path:"img\\10.jpeg",
+    reference:"A110689",
+    marque:"DENCKERMANN",
+    cout:"65.00"
+  },
+  model_piece={
+    designation:"FILTRE A HUILE GOLF5",
+    prix:"35.00",
+    image_path:"img\\11.jpeg",
+    reference:"A210079",
+    marque:"DENCKERMANN",
+    cout:"28.00"
+  },
+  model_piece={
+    designation:"FILTRE A HUILE GOLF 4",
+    prix:"33.00",
+    image_path:"img\\12.jpeg",
+    reference:"A210019",
+    marque:"DENCKERMANN",
+    cout:"28.00"
+  },
+  model_piece={
+    designation:"FILTRE A GAZOIL COLEOS/QUACHQAI",
+    prix:"145.00",
+    image_path:"img\\14.jpeg",
+    reference:"A120935",
+    marque:"DENCKERMANN",
+    cout:"120.00"
+  },
+  model_piece={
+    designation:"FILTRE A HUILE MER T203/T204",
+    prix:"40.00",
+    image_path:"img\\15.jpeg",
+    reference:"A210264",
+    marque:"DENCKERMANN",
+    cout:"28.00"
+  },
+  model_piece={
+    designation:"FILTRE A AIR DOKER(ADAPTABLE)",
+    prix:"25.00",
+    image_path:"img\\18.jpeg",
+    reference:"16546-7674R",
+    marque:"RENAULT",
+    cout:"20.00"
+  },
+model_piece={
+  designation:"FILTRE A AIR MEGANE 4",
+  prix:"140.00",
+  image_path:"img\\17.jpeg",
+  reference:"165468296R",
+  marque:"RENAULT",
+  cout:"115.00"
+},
+  model_piece={
+    designation:"FILTRE A HUILE DOBLO (ADAPTABLE)",
+    prix:"22.00",
+    image_path:"img\\19.jpeg",
+    reference:"71754237",
+    marque:"-",
+    cout:"20.00"
+  },
+  model_piece={
+    designation:"FILTRE A GASOIL KANGO2",
+    prix:"120.00",
+    image_path:"img\\20.jpeg",
+    reference:"71754237",
+    marque:"RENAULT",
+    cout:"74.95"
+  },
+
+  model_piece={
+    designation:"FILTRE A GAZOIL PUNTO(PM)",
+    prix:"40.00",
+    image_path:"img\\22.jpeg",
+    reference:"77362340",
+    marque:"-",
+    cout:"23.00"
+  },
+  model_piece={
+    designation:"FILTRE A AIR GOLF 4 1.9 /AUDI A3",
+    prix:"50.00",
+    image_path:"img\\26.jpeg",
+    reference:"A140012",
+    marque:"DENCKERMANN",
+    cout:"40.00"
+  },
+  model_piece={
+    designation:"FILTRE A GAZOIL M250/M190",
+    prix:"40.00",
+    image_path:"img\\27.jpeg",
+    reference:"A120006",
+    marque:"DENKERMAN",
+    cout:"32.00"
+  },
+  model_piece={
+    designation:"FILTER A AIR GOLF5/GOLF6",
+    prix:"65.00",
+    image_path:"img\\28.jpeg",
+    reference:"A140460",
+    marque:"DENCKERMANN",
+    cout:"55.00"
+  },
+  model_piece={
+    designation:"FILTRE BOITE AUTO MERCEDESS MEYLE",
+    prix:"100.00",
+    image_path:"img\\30.jpeg",
+    reference:"0140272005",
+    marque:"",
+    cout:"96.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE CLIO4/CLIO5 RENAULT",
+    prix:"110.00",
+    image_path:"img\\32.jpeg",
+    reference:"272773151R",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE CLIO3/DUSTER *CARBON",
+    prix:"110.00",
+    image_path:"img\\33.jpeg",
+    reference:"8201370532",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE MEGANE 2",
+    prix:"90.00",
+    image_path:"img\\34.jpeg",
+    reference:"7701064235",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTER HABITACLE MASTER 3",
+    prix:"100.00",
+    image_path:"img\\35.jpeg",
+    reference:"8201676037",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE KADJER",
+    prix:"100.00",
+    image_path:"img\\36.jpeg",
+    reference:"272774812R",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE FLUENCE /NISSAN JUKE",
+    prix:"110.00",
+    image_path:"img\\37.jpeg",
+    reference:"272774936R",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE TRFIC 3 /SCENIC 3",
+    prix:"100.00",
+    image_path:"img\\38.jpeg",
+    reference:"272778980R",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"FILTRE HABITACLE CLIO3/DUSTER",
+    prix:"90.00",
+    image_path:"img\\40.jpeg",
+    reference:"8201153808",
+    marque:"RENAULT",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"PLAQUETTE ARGOLF4/PEUGEOT",
+    prix:"85.00",
+    image_path:"img\\41.jpeg",
+    reference:"RH301121",
+    marque:"RH",
+    cout:"65.00"
+  },
+  model_piece={
+    designation:"JEUX PLAQUETTE (AR) M240",
+    prix:"50.00",
+    image_path:"img\\42.jpeg",
+    reference:"RH30910",
+    marque:"RH",
+    cout:"35.00"
+  },
+  model_piece={
+    designation:"PLAQUETTE AV LOGAN 1 RH /2",
+    prix:"90.00",
+    image_path:"img\\44.jpeg",
+    reference:"RH30400",
+    marque:"RH",
+    cout:"65.00"
+  },
+  model_piece={
+    designation:"SUPPORT MOTEUR T204 (D)",
+    prix:"550.00",
+    image_path:"img\\48.jpeg",
+    reference:"LM3557401",
+    marque:"LEMFORDER",
+    cout:"505.00"
+  },
+  model_piece={
+    designation:"SUPPORT MOTEUR T204 (G)",
+    prix:"550.00",
+    image_path:"img\\52.jpeg",
+    reference:"LM3557301",
+    marque:"LEMFORDER",
+    cout:"505.00"
+  },
+  model_piece={
+    designation:"ROULEMENT SUPPORT AMORTISSEUR GOLF5",
+    prix:"75.00",
+    image_path:"img\\51.jpeg",
+    reference:"3176401",
+    marque:"LEMFORDER",
+    cout:"65.00"
+  },
+  model_piece={
+    designation:"SUPPORT BOITE T203",
+    prix:"180.00",
+    image_path:"img\\53.jpeg",
+    reference:"3387501",
+    marque:"LEMFORDER",
+    cout:"140.00"
+  },
+  model_piece={
+    designation:"POMPE D'AMORSSAGE",
+    prix:"50.00",
+    image_path:"img\\23.jpeg",
+    reference:"27130385/2",
+    marque:"DECARBON",
+    cout:"33.00"
+  },
+  model_piece={
+    designation:"SUPPORT MOTEUR MERCEDESS T205 GAUCHE",
+    prix:"650.00",
+    image_path:"img\\45.jpeg",
+    reference:"3886901",
+    marque:"LEMFORDER",
+    cout:"575.00"
+  },
+  model_piece={
+    designation:"SUPPORT MOTEUR MERCEDESS T205 DROIT",
+    prix:"650.00",
+    image_path:"img\\47.jpeg",
+    reference:"3886801",
+    marque:"LEMFORDER",
+    cout:"575.00"
+  },
+  model_piece={
+    designation:"FILTRE A GAZOIL DOBLO MAXI",
+    prix:"100.00",
+    image_path:"img\\21.jpeg",
+    reference:"77366565",
+    marque:"-",
+    cout:"70.00"
+  },
+  model_piece={
+    designation:"COMMANDE LUMIERE DOKKER",
+    prix:"250.00",
+    image_path:"img\\24.jpeg",
+    reference:"8201167981/PPGF30/8201167988",
+    marque:"RENAULT",
+    cout:"120.00"
+  },
+  model_piece={
+    designation:"POLIE DCI &65",
+    prix:"55.00",
+    image_path:"img\\25.jpeg",
+    reference:"8200048486/8200276361/2",
+    marque:"RENAULT",
+    cout:"27.00"
+  },
+  model_piece={
+    designation:"PLAQUETTE FREIN AV DOKER/LOGAN",
+    prix:"80.00",
+    image_path:"img\\43.jpeg",
+    reference:"RH303332",
+    marque:"RH",
+    cout:"67.50"
+  },
+  model_piece={
+    designation:"SUPPORT MOTEUR M190",
+    prix:"200.00",
+    image_path:"img\\46.jpeg",
+    reference:"29042840A/1242401717",
+    marque:"BIGS",
+    cout:"180.00"
+  },
+
+
+
+]
+model_piece={
+  designation:"",
+  prix:"",
+  image_path:"img\\.jpeg",
+  reference:"",
+  marque:"",
+  cout:""
+}
+
+
+
+function afficher_le_catalogue(){
+  table= document.getElementById('catalogue_dynamique');
+  for(element of liste_de_piece){
+    var nouvelleLigne = table.insertRow(-1);
+    nouvelleLigne.id=element.designation+' '+element.reference;
+    //image
+    var nouvelleCellule = nouvelleLigne.insertCell(-1);
+    var img = document.createElement('img');
+    img.className='Icones'
+    img.src = element.image_path;
+    nouvelleCellule.appendChild(img);
+    //designation
+    var nouvelleCellule = nouvelleLigne.insertCell(-1);
+    var nouveauTexte = document.createTextNode(element.designation)
+    nouvelleCellule.appendChild(nouveauTexte);
+    //reference
+    var nouvelleCellule = nouvelleLigne.insertCell(-1);
+    var nouveauTexte = document.createTextNode(element.reference)
+    nouvelleCellule.appendChild(nouveauTexte);
+    //marque
+    var nouvelleCellule = nouvelleLigne.insertCell(-1);
+    var nouveauTexte = document.createTextNode(element.marque)
+    nouvelleCellule.appendChild(nouveauTexte);
+    //prix
+    var nouvelleCellule = nouvelleLigne.insertCell(-1);
+    var nouveauTexte = document.createTextNode(element.prix +' DH')
+    nouvelleCellule.appendChild(nouveauTexte);
+
+
+
+  }
+}
+
+function myFunction() {
+// Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("catalogue_dynamique");
+  tr = table.getElementsByTagName("tr");
+
+// Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = tr[i].id;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+      }
+      else {
+      tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
